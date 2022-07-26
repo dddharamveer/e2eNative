bec68df200b658b727af0cd8abf2ba9943bd3908
Dharamveer <workfordharamveer@gmail.com>
2022-07-26 14:49:11 +0530

    me

--- App.tsx
+++ App.tsx
@@ -1,18 +1,18 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
+  
   return (
     <View style={styles.container}>
-      <Text>I am done</Text>
-      <StatusBar style="auto" />
+    
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
