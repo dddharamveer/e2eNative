2fbf28ef2846e00cf580a068313cc36f46ebab1f
Bharat1111 <54268396+Bharat1111@users.noreply.github.com>
2022-07-26 18:24:10 +0530

    Home page

--- App.tsx
+++ App.tsx
@@ -1,20 +1,18 @@
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
 import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import HomePage from "./screens/HomePage";
 
 export default function App() {
-  
   return (
     <View style={styles.container}>
-    
+      <HomePage />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    flex: 1,
   },
 });
