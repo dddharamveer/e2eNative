c23019627348ea5acd32de6ab09d6ea4d1a34b38
Dharamveer singh <dharamveers162@gmail.com>
2022-08-02 22:30:05 +0530

    tabs+profile(noErr) 👷

--- navigation/Main.tsx
+++ navigation/Main.tsx
@@ -1,10 +1,11 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import PhoneNumberAuth from "../screens/PhoneNumberAuth";
+import Haram from "./BottomNavigation";
 const stack = createNativeStackNavigator();
 function Main() {
   return (
     <stack.Navigator>
-      <stack.Screen name="Auth" component={PhoneNumberAuth} />
+      <stack.Screen name="Auth" component={Haram} />
     </stack.Navigator>
   );
 }
