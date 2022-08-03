beeaa044663d03a50b056ae1425cbca661a44a57
Bharat1111 <54268396+Bharat1111@users.noreply.github.com>
2022-08-03 16:19:40 +0530

    context

--- App.tsx
+++ App.tsx
@@ -1,53 +1,30 @@
 import { StatusBar } from "expo-status-bar";
-import { useState } from "react";
-
 import { StyleSheet, Text, View } from "react-native";
-
-import { NavigationContainer } from "@react-navigation/native";
-
 import { useFonts } from "@expo-google-fonts/rubik";
-import AppLoading from "expo-app-loading";
-import { onAuthStateChanged } from "firebase/auth/react-native";
-
-import { auth } from "./constants/firebase/auth";
-import Auth from "./navigation/Auth";
-import Main from "./navigation/Main";
 import { rubik } from "./constants/fonts";
 
+import AuthProvider from "./store/authContext";
+
 export default function App() {
-  const [user, setuser] = useState(false);
+    let [fontsLoaded] = useFonts(rubik);
 
-  onAuthStateChanged(auth, (user) => {
-    if (user) {
-      setuser(true);
-    } else {
-      setuser(false);
+    if (!fontsLoaded) {
+        return <Text>dsaf</Text>;
     }
-  });
-  let [fontsLoaded] = useFonts(rubik);
 
-  if (!fontsLoaded) {
-    return <Text>dsaf</Text>;
-  }
-
-  let content = <Auth />;
-
-  if (user) {
-    content = <Main />;
-  }
-  return (
-    <>
-      <StatusBar style="light" />
-      <View style={styles.container}>
-        <NavigationContainer>{content}</NavigationContainer>
-      </View>
-    </>
-  );
+    return (
+        <>
+            <StatusBar style="light" />
+            <AuthProvider>
+                <View style={styles.container}></View>
+            </AuthProvider>
+        </>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "white",
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "white",
+    },
 });
